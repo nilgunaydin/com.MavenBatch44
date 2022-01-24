4a2830d713c4f6fc49cd1abcb8b4ea9f25872a58
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2022-01-24 16:47:30 +0300

    hw commit

--- src/test/java/odev/odev1.java
+++ src/test/java/odev/odev1.java
@@ -1,18 +1,24 @@
 package odev;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class odev1 {
-    //Question1
-    //1.https://www.youtube.com sayfasına gidin
-    //2.Title i consolda yazdirin
-    //3.Title “YouTube” a eşitse “correct title” yazdirin değilse “incorrect title yzdirin”
-    //4.Sayfayi kapatin
-    //5.Tum sayfalari kapatin
-    //6.Consola “Test completed” yazdirin
     public static void main(String[] args) {
-
-
-
-
-
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.youtube.com");
+        //2.Title i consolda yazdirin
+        System.out.println(driver.getTitle());
+        //3.Title “YouTube” a eşitse “correct title” yazdirin değilse “incorrect title yzdirin”
+  if(driver.getTitle().equals("YouTube")) System.out.println("Correct Title");
+  else System.out.println("Incorrect Title");
+        //4.Sayfayi kapatin
+        driver.close();
+        //5.Tum sayfalari kapatin
+        driver.quit();
+        //6.Consola “Test completed” yazdirin
+        System.out.println("Test Completed");
     }
 }
